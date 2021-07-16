c8195bec0c724382d99d922bb090c2a77ee2130e
Ritik <ritikvverma@gmail.com>
2021-07-16 18:01:22 +0530

    mvvm,databinding,dependency injection

---
+++ xamarin_mvvm/MonkeyFinder/MonkeyFinder/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using MonkeyFinder.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Device = Xamarin.Forms.Device;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MonkeyFinder
+{
+    public partial class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            On<Windows>().SetImageDirectory("Assets");
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = (Color)Resources["Primary"],
+                BarTextColor = Color.White
+            };
+        }
+
+        const string AppCenteriOS = "";
+        const string AppCenterAndroid = "";
+
+        protected override void OnStart()
+        {
+            if (!string.IsNullOrWhiteSpace(AppCenteriOS) && !string.IsNullOrWhiteSpace(AppCenterAndroid))
+            {
+                AppCenter.Start($"ios={AppCenteriOS};" +
+                      $"android={AppCenterAndroid}",
+                      typeof(Analytics), typeof(Crashes));
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ xamarin_mvvm/MonkeyFinder/MonkeyFinder/View/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MonkeyFinder.Model;
+using MonkeyFinder.ViewModel;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace MonkeyFinder.View
+{
+    public partial class MainPage : ContentPage
+    {
+        MonkeysViewModel model;
+        public MainPage()
+        {
+            InitializeComponent();
+            model = new MonkeysViewModel();
+            /*BindingContext = new MonkeysViewModel();*/
+            BindingContext = model;
+
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            model.GetMonkeysCommand.Execute(null);
+
+        }
+
+        private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var monkey = e.CurrentSelection.FirstOrDefault() as Monkey;
+            if (monkey == null)
+                return;
+
+            await Navigation.PushAsync(new DetailsPage(monkey));
+
+            ((CollectionView)sender).SelectedItem = null;
+        }
+    }
+}
